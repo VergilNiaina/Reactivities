ae66c84356ab1d513f051f1531cf9be48a58fb0c
Manda Niaina Finaritra Rasolofomanantsoa Andrianina <mrasolofomanantsoaan@webhelp.fr>
2023-07-14 09:38:52 +0300

    Section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -29,6 +29,17 @@ export default class ActivityStore{
           return Array.from(this.activityRegistry.values()).sort((a,b) => 
                     Date.parse(a.dateTime) - Date.parse(b.dateTime))
      }
+     // computed
+     get groupedActivities(){
+          return Object.entries(
+               this.activitiesByDate.reduce((activities, activity) => {
+                    const date = activity.dateTime;
+                    activities[date] = activities[date] ?[...activities[date], activity] : [activity]
+                    return activities;
+               },{} as {[key:string]: Activity[]})
+          )
+     }
+
      // Action 
      loadActivities = async () =>{
           if(!this.loadingInitial) this.setLoadingInitial(true)
